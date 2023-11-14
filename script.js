e37f9c5c4567389a8b66505c207456eae0f0ab51
hendricksonweib <hendricksonrocha22@gmail.com>
2023-11-14 08:51:32 -0300

    fix javascript
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const ligar = document.getElementById('lig')
-const desligar = document.getElementById('desl')
 const lamp = document.getElementById('lamp')
 const img = document.getElementsByTagName('img')
-
+const main = document.getElementsByTagName('main')
+const body = document.body
+const titulo = document.getElementById('titulo')
 ligar.addEventListener('click', function(){
-    lamp.src= "ligada.jpg"
-}
-)
+    if(lamp.src.endsWith("desligada.jpg")){
+       lamp.src="ligada.jpg"
+       ligar.innerText ='Desligar'
+       body.style.backgroundColor = 'black'
+       titulo.style.color = 'white'
+    }else{
+        lamp.src= "desligada.jpg"
+        ligar.innerText ='Ligar'
+        body.style.backgroundColor = 'white'
+        titulo.style.color = 'black'
+    }
+})
 
-desligar.addEventListener('click', function(){
-    lamp.src= "desligada.jpg"
-}
-)
